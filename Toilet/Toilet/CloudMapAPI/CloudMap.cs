fbd8b228a738db91e5c671b182ddff2516b2edf5
RaulVan <vanraul@outlook.com>
2014-04-01 00:06:19 +0800

    --

---
+++ Toilet/Toilet/CloudMapAPI/CloudMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toilet.CloudMapAPI
+{
+    public class CloudMap
+    {
+        //上传数据
+
+        public static async Task<string> CreateDataAsync(CloudOptions options)
+        {
+
+            return await HttpConnect.PostStringAsync(options.Datas, CloudMapConfig.UrlCreate, "application/x-www-form-urluncoded");
+        }
+
+        //查询数据
+
+        //修改数据
+
+
+    }
+}
---
+++ Toilet/Toilet/CloudMapAPI/CloudOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toilet.CloudMapAPI
+{
+    public class CloudOptions
+    {
+        public string Id;
+
+        public string TableId;
+        //public string 
+        public string Datas;
+    }
+}
