4c9d20271d8c9c5a4df6cb903e885c4f94dfbdff
RaulVan <vanraul@outlook.com>
2014-03-31 23:56:24 +0800

    添加云图API

---
+++ Toilet/Toilet/CloudMapAPI/CloudMapConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toilet.CloudMapAPI
+{
+  internal  class CloudMapConfig
+    {
+        //POST
+        public static string UrlCreate = "http://yuntuapi.amap.com/datamanage/data/create";
+        public static string UrlBatchCreate = "http://yuntuapi.amap.com/datamanage/data/batchcreate";
+        public static string UrlUpdate = "http://yuntuapi.amap.com/datamanage/data/update";
+        public static string UrlDelete = "http://yuntuapi.amap.com/datamanage/ data/delete";
+        //GET
+        public static string UrlList = "http://yuntuapi.amap.com/datamanage/ data/list?";
+        public static string UrlImportStatus = "http://yuntuapi.amap.com/datamanage/batch/importstatus?";
+
+
+        public static string UrlAround = "http://yuntuapi.amap.com/datasearch/around?";
+        public static string UrlPolygon = "http://yuntuapi.amap.com/datasearch/polygon?";
+        public static string UrlId = "http://yuntuapi.amap.com/datasearch/id?";
+
+    }
+}
---
+++ Toilet/Toilet/CloudMapAPI/HttpConnect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toilet.CloudMapAPI
+{
+   internal class HttpConnect
+    {
+       public static async Task<string> PostStringAsync(string data, string uri, string contenType)
+       {
+           HttpClient client = new HttpClient();
+           HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, uri);
+
+
+           request.Content = new StringContent(data, Encoding.UTF8, contenType);
+           HttpResponseMessage response = await client.SendAsync(request);
+           string responseString = await response.Content.ReadAsStringAsync();
+           return responseString;
+       }
+
+       public static async Task<string> GetStringAsync(string data, string uri)
+       {
+           HttpClient client = new HttpClient();
+           HttpResponseMessage response = await client.GetAsync(string.Format("{0}{1}", uri, data));
+           string responseString = await response.Content.ReadAsStringAsync();
+           //TODO:判断返回的status，
+
+
+           return responseString;
+       }
+
+    }
+}
